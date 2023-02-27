86d11b5b822a7eb1b228f6480024a5d528a2bb33
Rajendra Kumar Shaw <55447184+rajendrakrshaw@users.noreply.github.com>
2023-02-27 15:55:34 +0530

    Add files via upload

---
+++ sem2/java/assingment4/que16/Temperature.java
@@ -0,0 +1,22 @@
+
+import java.utils.*;
+
+public class Temperature {
+    static public menu(){
+        System.out.println("Welcome to temperature converter...");
+        System.out.println("_______________MENU________________");
+        System.out.println("1. Centigrade to Farenheit: ");
+        System.out.println("2. Farenheit to Centgrade.");
+        System.out.println("3. Exit.");
+    }
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    bool t = true;
+    while(t){
+        clrscr();
+        menu();
+        
+    }
+   }
+    
+}
